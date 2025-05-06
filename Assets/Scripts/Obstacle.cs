ebcec02406e394e4b94e7ea700ec74ca2ce48bdf
Alex Cobas <alexcobasfp@ibf.cat>
2025-05-06 16:44:29 +0200

    Add spikes and die logic

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerColision player = other.GetComponent<PlayerColision>();
+            if (player != null)
+            {
+                player.Die();
+            }
+        }
+    }
+}
