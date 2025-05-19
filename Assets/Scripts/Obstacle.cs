2397fb4e5a0aba6266f10adc4fd00a9f03234a23
davidragus <davidruizfp23@ibf.cat>
2025-05-19 19:04:04 +0200

    fix: Fix player death behaviour

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,28 +2,28 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // protected virtual void OnTriggerEnter(Collider other)
-    // {
-    //     if (other.CompareTag("Player"))
-    //     {
-    //         PlayerColision player = other.GetComponent<PlayerColision>();
-    //         if (player != null)
-    //         {
-    //             player.Die();
-    //         }
-    //     }
-    // }
+	// protected virtual void OnTriggerEnter(Collider other)
+	// {
+	//     if (other.CompareTag("Player"))
+	//     {
+	//         PlayerColision player = other.GetComponent<PlayerColision>();
+	//         if (player != null)
+	//         {
+	//             player.Die();
+	//         }
+	//     }
+	// }
 
-    protected virtual void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            PlayerColision player = other.GetComponent<PlayerColision>();
-            if (player != null)
-            {
-                player.Die();
-            }
-        }
-    }
-    
+	protected virtual void OnTriggerStay(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			PlayerColision playerCollision = other.GetComponent<PlayerColision>();
+			if (playerCollision != null)
+			{
+				playerCollision.Die();
+			}
+		}
+	}
+
 }
